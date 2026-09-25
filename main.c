10f130324447c677656ae2e85b058be5d1961be2
Rayane <rayanebenlechhab2006@gmail.com>
2026-09-25 16:09:15 +0200

    Initialisation du Mini File Explorer

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("============================\n");
+    printf("      MINI FILE EXPLORER\n");
+    printf("============================\n");
+
+    printf("\nBienvenue dans Mini File Explorer!\n");
+
+    return 0;
+}
